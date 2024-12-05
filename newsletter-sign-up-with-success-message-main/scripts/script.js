d268eca793cde26770a3727b14d27232fa1e8f20
@Omner3 <gonzalezomner@hotmail.com>
2024-12-04 20:36:02 -0600

    validación de email agragada

--- newsletter-sign-up-with-success-message-main/scripts/script.js
+++ newsletter-sign-up-with-success-message-main/scripts/script.js
@@ -2,7 +2,7 @@ const subscribe = document.querySelector('.subscribe')
 const ventana = document.querySelector('.ventana')
 const cerrarVentana = document.querySelector('.cerrar-ventana')
 const email = document.querySelector('.form-control')
-
+const input = document.querySelector('#user-email')
 
 subscribe.addEventListener('click',() => {
     ventana.classList.add('mostrar-ventana')
@@ -10,4 +10,34 @@ subscribe.addEventListener('click',() => {
 
 cerrarVentana.addEventListener('click',() => {
     ventana.classList.remove('mostrar-ventana')
+})
+
+function validateEmail(){
+                
+	// Get our input reference.
+	var emailField = document.getElementById('user-email');
+	
+	// Define our regular expression.
+	var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+	// Using test we can check if the text match the pattern
+	if( validEmail.test(emailField.value) ){
+		return true;
+	}else{
+		alert('Email is invalid, skip form submission');
+        const invalid = document.createElement('div')
+        invalid.classList = "invalid-feedback"
+        invalid.textContent = "Valid email required"
+        input.appendChild = (invalid)
+		return false;
+	}
+}
+
+const successMessageEmail = document.querySelector('.ventana strong') 
+
+subscribe.addEventListener('click', () => {
+  ventana.classList.add('mostrar-ventana')
+
+  
+  successMessageEmail.textContent = input.value
 })
